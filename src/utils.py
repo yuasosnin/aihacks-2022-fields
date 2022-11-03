f156716f8346d32e583fe1c5ac22a6146c1c2522
Yuriy Sosnin <sosnin.yuriy@gmail.com>
2022-11-03 22:52:56 +0300

    First working solution

---
+++ src/utils.py
@@ -0,0 +1,52 @@
+import geojson
+import shapely.geometry
+import pandas as pd
+import geopandas as gpd
+
+
+def parse_geometry(geom_str):
+    try:
+        return shapely.geometry.shape(geojson.loads(geom_str))
+    except (TypeError, AttributeError):  # Handle NaN and empty strings
+        return None
+
+    
+def handle_bad_geometry(geometry):
+    if isinstance(geometry, shapely.geometry.GeometryCollection):
+        for geom in geometry.geoms:
+            if isinstance(geom, shapely.geometry.Polygon):
+                return geom
+        else:
+            return None
+    else:
+        return geometry
+    
+    
+def read_data(filename):
+    df = pd.read_csv(filename)
+    df['geometry'] = df['.geo'].apply(parse_geometry).apply(handle_bad_geometry)
+    return gpd.GeoDataFrame(df, crs=4326)
+
+
+def process_data(data):
+    try:
+        data_ts = data.drop(['id', 'area', '.geo', 'crop', 'geometry'], axis=1)
+    except KeyError:
+        data_ts = data.drop(['id', 'area', '.geo', 'geometry'], axis=1)
+    data_ts.columns = pd.to_datetime(pd.to_datetime([x[8:] for x in data_ts.columns]))
+    data_ts = data_ts[sorted(data_ts.columns)]
+    try:
+        data_id = data[['id', 'area', 'crop', 'geometry']]
+    except KeyError:
+        data_id = data[['id', 'area', 'geometry']]
+        
+    data_id['centroid'] = data_id['geometry'].to_crs(4327).centroid
+    data_id['lat'] = data_id['centroid'].apply(lambda x: x.coords[0][0])
+    data_id['lon'] = data_id['centroid'].apply(lambda x: x.coords[0][1])
+    data_id = data_id.drop('centroid', axis=1)
+    
+    return data_ts, data_id
+
+
+def ndvi(red, nir):
+    return (nir-red)/(nir+red)
